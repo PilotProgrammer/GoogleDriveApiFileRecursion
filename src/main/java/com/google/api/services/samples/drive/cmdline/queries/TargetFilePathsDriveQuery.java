1eb68077f0aa577ebbbb1f390f667e1689fc4be1
PilotProgrammer <twogsw@yahoo.com>
2019-03-02 12:19:33 -0600

    WIP

--- src/main/java/com/google/api/services/samples/drive/cmdline/queries/TargetFilePathsDriveQuery.java
+++ src/main/java/com/google/api/services/samples/drive/cmdline/queries/TargetFilePathsDriveQuery.java
@@ -9,9 +9,13 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 
 public class TargetFilePathsDriveQuery {
 	protected Drive service = null;
@@ -53,46 +57,40 @@ public class TargetFilePathsDriveQuery {
 			// 4
 			ReverseNode rootNode = new ReverseNode();
 			rootNode.currentItem = targetFile;
-			// 5
+			// 5 -- this data structure can have multiple "branches", as the "parentItems property of a reverseNode is a list
 			constructReverseFilePath(rootNode);
 			allReverseFilePaths.add(rootNode);
 		}
 	
-		List<Node> allForwardFilePaths = new ArrayList<Node>();
+//		List<Node> allForwardFilePaths = new ArrayList<Node>();
+//		allReverseFilePaths.forEach((reverseNode) -> {
+//			// when you encounter MULTIPLE parents, you need to COPY the forwardNode branch as it is, and then 
+//		});
+
+		toBranches();
 		
-		allReverseFilePaths.forEach((reverseNode) -> {
-			
-			// when you encounter MULTIPLE parents, you need to COPY the forwardNode branch as it is, and then 
-		});
 
 		return allReverseFilePaths;
 	}
 	
-	public List<List<File>> toBranches(Queue<String> targetFilePath) throws IOException {
+	public void toBranches() throws IOException {
+		Queue<String> targetFilePath = null;
 		String targetFileNameFromQueue = targetFilePath.poll();
 
-		if (!StringUtils.equals(targetFileNameFromQueue, targetFileName)) {
-			throw new IllegalArgumentException(String.format(
-					"The first item in the search queue is the target file: %s "
-							+ "This target file in the search queue does not match the target search file that was provided during object instantiation: %s",
-					targetFileNameFromQueue, targetFileName));
-		}
-
-		List<ReverseNode> nodeList = findFilePaths();
+		Map<File,Set<List<File>>> allForwardFilePaths = new HashMap<File,Set<List<File>>>();
+		
 		List<File> returnTargetFilePath = new LinkedList<File>();
 
-		if (nodeList != null && !nodeList.isEmpty()) {
-			for (ReverseNode firstNode : nodeList) {
-				// the targetFilePath could have multiple results, and we call
-				// validateTargetFilePath on each one.
-				// because of this, we need to COPY the queue, as the
-				// initial call to validateTargetFilePath for each new firstNode
-				// consumes the queue it receives.
+		if (allReverseFilePaths != null && !allReverseFilePaths.isEmpty()) {
+			for (ReverseNode targetFileNode : allReverseFilePaths) {
+				File targetFile = targetFileNode.currentItem;
+				
+				Set<List<File>> setOfForwardPathsForTargetFile = new HashSet<List<File>>();
 				Queue<String> queueToConsume = new LinkedList<String>(targetFilePath);
-				List<File> subsequentTargetFilePath = validateTargetFilePath(queueToConsume, firstNode);
+				List<File> subsequentTargetFilePath = toBranches2(queueToConsume, targetFileNode);
 
 				if (CollectionUtils.isNotEmpty(subsequentTargetFilePath)) {
-					returnTargetFilePath.add(firstNode.currentItem);
+					returnTargetFilePath.add(targetFileNode.currentItem);
 					returnTargetFilePath.addAll(subsequentTargetFilePath);
 					break;
 				}
@@ -103,7 +101,6 @@ public class TargetFilePathsDriveQuery {
 			}
 		}
 
-		return null;
 	}
 	
 	protected List<File> toBranches2(Queue<String> targetFileNameFromQueue, ReverseNode currentNode)
@@ -123,19 +120,7 @@ public class TargetFilePathsDriveQuery {
 					returnTargetFilePath.addAll(subsequentFileList);
 					break;
 				}
-				/*
-				 * why queue.isEmpty()? because the following situation is possible and would
-				 * produce a false positive if we didn't check for the queue being empty. say
-				 * the file exists under a directory structure of [root] -> D -> E -> F ->
-				 * [targetFile], and furthermore say that the search queue has structure of
-				 * [root] -> A-> B-> C -> D -> E -> F -> [targetFile]. Well, as soon as we
-				 * reverseFileCompare runs and we get D, then next recursive call of
-				 * reverseFileCompare will end on [root], and the recursion will end. However,
-				 * the recursive search in that case would have incorrectly excluded A, B, and C
-				 * from the check. By having the queue.isempty check, we ensure that this
-				 * situation will not happen, as the queue isn't empty until A, B, and C are
-				 * successfully taken off the queue.
-				 */
+
 			} else if (rootFolder.getId().equals(parentFolder.getId()) && targetFileNameFromQueue.isEmpty()) {
 				returnTargetFilePath.add(rootFolder);
 				break;
